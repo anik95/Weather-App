812dc2a9858b49075b9d2095d1ea724c3d24c0c7
anik95 <mustadirmahmood840@gmail.com>
2021-04-21 15:46:53 +0600

    weather app

---
+++ src/store/actions/weather.js
@@ -0,0 +1,12 @@
+import * as actionTypes from './actionTypes'
+
+export const incrementCounter = () => {
+  return {
+    type: actionTypes.INCREMENT_SUBMIT_COUNT
+  }
+}
+export const updateSearch = () => {
+  return {
+    type: actionTypes.UPDATE_SEARCH
+  }
+}
